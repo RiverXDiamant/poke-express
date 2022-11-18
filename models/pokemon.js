a7c7c7d1ad04a019ecf5c75385da031506824a6a
Alex <eddings.sharanna@gmail.com>
2022-11-17 20:24:09 -0500

    updated to include pokemon photos

--- models/pokemon.js
+++ models/pokemon.js
@@ -2,30 +2,33 @@
 
 const pokemon = [
   {
-    name: "bulbasaur",
-    img: "https://duckduckgo.com/?q=bulbasaur&t=newext&atb=v342-1&iax=images&ia=images&iai=https%3A%2F%2Fimages.hdqwalls.com%2Fdownload%2Fbulbasaur-pokemon-dt-1600x1200.jpg",
+    name: "Bulbasaur",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages-wixmp-ed30a86b8c4ca887773594c2.wixmp.com%2Ff%2F1f6e6960-47bd-455f-9411-6d98b2bb5d26%2Fdd49wrr-c20af9ba-e18e-4c59-8558-b5705acf6f24.png%3Ftoken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvMWY2ZTY5NjAtNDdiZC00NTVmLTk0MTEtNmQ5OGIyYmI1ZDI2XC9kZDQ5d3JyLWMyMGFmOWJhLWUxOGUtNGM1OS04NTU4LWI1NzA1YWNmNmYyNC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.1hXU_mg8OXBv699eBboCzQVO-Qidl5oXxjFGIIzazec&f=1&nofb=1&ipt=cb602729bc721349b00738dab3d6178d8af48b66422acfd4f3f61da6c38c2494&ipo=images",
   },
   {
-    name: "ivysaur",
-    img: "https://i.pinimg.com/originals/e7/bf/06/e7bf06f644f54fc70108386d6dd171e7.jpg",
+    name: "Ivysaur",
+    img: "https://cdn.weasyl.com/~gminor/submissions/1844233/6ff51b2ffb1b27816133423341d2ec2571f1ce49955b26063f9f0b7889347108/gminor-random-pok-mon-4-ivysaur.png",
   },
   {
-    name: "venusaur",
+    name: "Venusaur",
     img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/intermediary/f/89c0f2de-f055-4265-bf89-2f198b527127/d8qk6vl-ee6fb9cd-cf27-4a3c-a3f0-7e0279448a7d.png/v1/fill/w_800,h_451,q_80,strp/day_352___mega_fushigibana___venusaur_by_autobottesla_d8qk6vl-fullview.jpg",
   },
   {
-    name: "charmander",
+    name: "Charmander",
     img: "https://cdna.artstation.com/p/assets/covers/images/029/612/488/large/joshua-dunlop-joshua-dunlop-charmander-2-watermarked.jpg?1598104305",
   },
   {
-    name: "charizard",
-    img: "https://cdna.artstation.com/p/assets/covers/images/029/612/488/large/joshua-dunlop-joshua-dunlop-charmander-2-watermarked.jpg?1598104305",
+    name: "Charizard",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.rankedboost.com%2Fwp-content%2Fuploads%2F2016%2F03%2Fcharizard-768x768.png&f=1&nofb=1&ipt=dc53324a01d07220376be8bf212bdda417cec362548651a2aa7d708a06eed1e4&ipo=images",
   },
   {
-    name: "squirtle",
-    img: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/7ef1e284-dc09-45bb-a1ee-d8c65a1ae170/d7dwh31-95fa41d0-7763-47d6-8916-71e8e0f5459e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi83ZWYxZTI4NC1kYzA5LTQ1YmItYTFlZS1kOGM2NWExYWUxNzAvZDdkd2gzMS05NWZhNDFkMC03NzYzLTQ3ZDYtODkxNi03MWU4ZTBmNTQ1OWUucG5nIn1dXX0.TF-inKUsyMIGk9hYrxwS7EXbJbKTCs2CfMIuiQdIwnI",
+    name: "Squirtle",
+    img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fmax%2F750%2F1*W_ITBT0_dDNGyy_yPRQO3g.jpeg&f=1&nofb=1&ipt=e90ef5f78651d06031de898b5052a5f1a2c13b042908e4d07045fe0502cb8a8b&ipo=images",
   },
-  { name: "wartortle", img: "https://salten.cz/wp-content/uploads/2021/07/040df-16268159914309-800.jpg" },
+  {
+    name: "Wartortle",
+    img: "https://salten.cz/wp-content/uploads/2021/07/040df-16268159914309-800.jpg",
+  },
 ];
 
 module.exports = pokemon;
