9b04c0323f9a98d4e2cd46f2dd615086753284e9
Alex <eddings.sharanna@gmail.com>
2022-11-19 13:57:14 -0500

    created pokemon schema

--- models/pokemon.js
+++ models/pokemon.js
@@ -1,13 +1,33 @@
+// TODO: - Part 2 with Pokedex
+// * require mongoose
+// * create pokemon schema
+// * add Model for pokemon
+// * export modules
+
 // "Database" set up
+const mongoose = require("mongoose");
 
-const pokemon = [
-  { name: "Bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur" },
-  { name: "Ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur" },
-  { name: "Venusaur", img: "http://img.pokemondb.net/artwork/venusaur" },
-  { name: "Charmander", img: "http://img.pokemondb.net/artwork/charmander" },
-  { name: "Charizard", img: "http://img.pokemondb.net/artwork/charizard" },
-  { name: "Squirtle", img: "http://img.pokemondb.net/artwork/squirtle" },
-  { name: "Wartortle", img: "http://img.pokemondb.net/artwork/wartortle" },
-];
+const pokemonSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  image: { type: String, required: true },
+});
 
-module.exports = pokemon;
+// All -model names- should be capitalized
+// Pokemon model
+
+const Pokedex = mongoose.model("Pokedex", pokemonSchema);
+
+// Export modules
+module.exports = Pokedex;
+
+// const pokemon = [
+//   { name: "bulbasaur", img: "http://img.pokemondb.net/artwork/bulbasaur" },
+//   { name: "ivysaur", img: "http://img.pokemondb.net/artwork/ivysaur" },
+//   { name: "venusaur", img: "http://img.pokemondb.net/artwork/venusaur" },
+//   { name: "charmander", img: "http://img.pokemondb.net/artwork/charmander" },
+//   { name: "charizard", img: "http://img.pokemondb.net/artwork/charizard" },
+//   { name: "squirtle", img: "http://img.pokemondb.net/artwork/squirtle" },
+//   { name: "wartortle", img: "http://img.pokemondb.net/artwork/wartortle" },
+// ];
+
+// module.exports = pokemon;
